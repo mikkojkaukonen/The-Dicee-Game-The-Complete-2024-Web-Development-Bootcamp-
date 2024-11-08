6feadc4f8b5a904090ee13593afef6b7b4d28bfb
Mikko Kaukonen <141821425+mikkojkaukonen@users.noreply.github.com>
2024-11-08 14:01:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
 
 // Generate a random number between 1 and 6 for players 1 & 2
-
 var randomNumber1 = Math.floor((Math.random()*6)+1);
-
 var randomNumber2 = Math.floor((Math.random()*6)+1);
-
 
 // Selects the first dice image and updates its source based on the random number for players 1 & 2
 var img1 = document.querySelector(".img1");
 img1.src = "./images/dice" + randomNumber1 + ".png";
-
 var img2 = document.querySelector(".img2");
 img2.src = "./images/dice" + randomNumber2 + ".png";
 
-
 // Compares the random numbers and updates the result in the heading
-
 if (randomNumber1 > randomNumber2) {
     document.querySelector("h1").innerHTML = "☝️ Player 1 Wins!";
 }
